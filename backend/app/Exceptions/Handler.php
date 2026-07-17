f220a57a16925b347865232eda05e405d85e867c
MD.Ubaid <mu0430218@gmail.com>
2026-07-17 17:07:51 +0530

    feat: setup Sentry for error monitoring

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -22,7 +22,7 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            \Sentry\Laravel\Integration::captureUnhandledException($e);
         });
     }
 }
